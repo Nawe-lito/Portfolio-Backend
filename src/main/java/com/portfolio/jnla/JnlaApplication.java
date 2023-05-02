ea3629ab8dc7297782aed621d0cd07473435d5a9
Nawe <nahue.ledesma@mi.unc.edu.ar>
2023-05-02 20:31:37 -0300

    base de datos levantada

---
+++ src/main/java/com/portfolio/jnla/JnlaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jnla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnlaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/portfolio/jnla/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+package com.portfolio.jnla.Repository;
+
+import com.portfolio.jnla.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
